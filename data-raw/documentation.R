2b269ad294272fa429a000d289becf1d02dbaf11
Shir Dekel <shirdekel@yahoo.com.au>
2021-05-16 11:51:00 +1000

    Add data

---
+++ data-raw/documentation.R
@@ -0,0 +1,75 @@
+#' aggregation4
+#' A data package for aggregation4.
+#' @docType package
+#' @aliases aggregation4-package
+#' @title Package Title
+#' @name aggregation4
+#' @description A description of the data package
+#' @details Use \code{data(package='aggregation4')$results[, 3]} to see a list of available data sets in this data package
+#'     and/or DataPackageR::load_all
+#' _datasets() to load them.
+#' @seealso
+#' \link{data}
+NULL
+
+
+
+#' Detailed description of the data
+#' @name data
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{tbl_df} containing the following fields:
+#' \describe{
+#' \item{id}{}
+#' \item{subject}{}
+#' \item{condition}{}
+#' \item{presentation}{}
+#' \item{distribution}{}
+#' \item{awareness}{}
+#' \item{proportion}{}
+#' \item{portfolio_binary}{}
+#' \item{sex}{}
+#' \item{age}{}
+#' \item{language}{}
+#' \item{language_other}{}
+#' \item{business_edu}{}
+#' \item{business_exp}{}
+#' \item{current}{}
+#' \item{sector}{}
+#' \item{sector_other}{}
+#' \item{employees}{}
+#' \item{revenue}{}
+#' \item{role_company}{}
+#' \item{role_company_other}{}
+#' \item{role_allocation}{}
+#' \item{role_allocation_other}{}
+#' \item{budget}{}
+#' \item{project_expectation}{}
+#' \item{project_number}{}
+#' \item{portfolio_number}{}
+#' \item{stage}{}
+#' \item{experiment}{}
+#' \item{sample}{}
+#' \item{similarity}{}
+#' \item{thesis_project}{}
+#' \item{project_order}{}
+#' \item{project}{}
+#' \item{detail}{}
+#' \item{outcome_positive}{}
+#' \item{outcome_dif}{}
+#' \item{probability_positive}{}
+#' \item{choice}{}
+#' \item{datetime}{}
+#' \item{total_time}{}
+#' \item{gamble}{}
+#' \item{expected_value}{}
+#' \item{gain_loss_ratio}{}
+#' \item{date_rank}{}
+#' }
+#' @source The data comes from________________________.
+#' @seealso
+#' \link{aggregation4}
+NULL
+
+
+
